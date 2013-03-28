acf863e0e545c5f7804b833ea5e1310883c4fc7f
Shawn Neal <sneal@sneal.net>
2013-03-28 12:45:34 -0700

    Removed status codes from errors, not sure they're still used

--- lib/vagrant-windows/errors.rb
+++ lib/vagrant-windows/errors.rb
@@ -1,20 +1,20 @@
 module Vagrant
   module Errors
     class WinRMPortNotDetected < VagrantError
-      status_code(550)
+      #status_code(550)
       error_key(:winrm_port_not_detected)
     end
 
     class WinRMInvalidShell < VagrantError
-      status_code(551)
+      #status_code(551)
       error_key(:winrm_invalid_shell)
     end
     class WinRMTransferError < VagrantError
-      status_code(552)
+      #status_code(552)
       error_key(:winrm_upload_error)
     end
     class WinRMTimeout < VagrantError
-      status_code(553)
+      #status_code(553)
       error_key(:winrm_timeout)
     end
 
