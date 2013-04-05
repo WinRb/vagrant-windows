b5b2c135a34de61b799a1327f2db6e71b52efaaf
Shawn Neal <sneal@sneal.net>
2013-04-04 18:26:21 -0700

    Fixed machine monkey patch so it doesn't break non-windows guests when installed

--- lib/vagrant-windows/monkey_patches/machine.rb
+++ lib/vagrant-windows/monkey_patches/machine.rb
@@ -1,20 +1,20 @@
 module Vagrant
   class Machine
     
-    #ssh_communicate = instance_method(:communicate)
+    ssh_communicate = instance_method(:communicate)
     
     # This patch is needed until Vagrant supports a configurable communication channel
-    #define_method(:communicate) do
-    #  if @guest.class.eql? VagrantPlugins::Windows::Guest
-    #    @communicator ||= Communication::WinRMCommunicator.new(self)
-    #  else
-    #    @communicator ||= ssh_communicate.bind(self).()
-    #  end
-    #  @communicator
-    #end
-    
-    def communicate
-      @communicator ||= Communication::WinRMCommunicator.new(self)
+    define_method(:communicate) do
+      unless @communicator
+        if @config.vm.guest.eql? :windows
+          @logger.info("guest is #{@config.vm.guest}, using WinRM for communication channel")
+          @communicator = Communication::WinRMCommunicator.new(self)
+        else
+          @logger.info("guest is #{@config.vm.guest}, using SSH for communication channel")
+          @communicator = ssh_communicate.bind(self).()
+        end
+      end
+      @communicator
     end
     
     def winrm
