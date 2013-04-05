89e25162517665cd91a574de53d4202cde379b78
Shawn Neal <sneal@sneal.net>
2013-04-05 14:54:12 -0700

    Bootstrap plugin logging to match the current Vagrant logging level

--- lib/vagrant-windows/plugin.rb
+++ lib/vagrant-windows/plugin.rb
@@ -37,6 +37,7 @@ module VagrantWindows
     DESC
     
     config(:windows) do
+      setup_logging()
       setup_i18n()
       VagrantWindows::Config::Windows
     end
@@ -53,7 +54,36 @@ module VagrantWindows
     def self.setup_i18n
       I18n.load_path << File.expand_path("locales/en.yml", VagrantWindows.source_root)
       I18n.reload!
-    end  
+    end
+    
+    # This sets up our log level to be whatever VAGRANT_LOG is.
+    def self.setup_logging
+      require "log4r"
+
+      level = nil
+      begin
+        level = Log4r.const_get(ENV["VAGRANT_LOG"].upcase)
+      rescue NameError
+        # This means that the logging constant wasn't found,
+        # which is fine. We just keep `level` as `nil`. But
+        # we tell the user.
+        level = nil
+      end
+
+      # Some constants, such as "true" resolve to booleans, so the
+      # above error checking doesn't catch it. This will check to make
+      # sure that the log level is an integer, as Log4r requires.
+      level = nil if !level.is_a?(Integer)
+
+      # Set the logging level on all "vagrant" namespaced
+      # logs as long as we have a valid level.
+      if level
+        logger = Log4r::Logger.new("vagrant_windows")
+        logger.outputters = Log4r::Outputter.stderr
+        logger.level = level
+        logger = nil
+      end
+    end
       
     #TODO:Puppet provisioner
   end
