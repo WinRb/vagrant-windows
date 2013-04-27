5d58fa025761dfeed25d3ac2a4d16945bc314ba3
Shawn Neal <sneal@sneal.net>
2013-04-26 19:55:46 -0700

    Fixed the which command to properly return an error exit code if the command does not exist

--- lib/vagrant-windows/scripts/command_alias.ps1
+++ lib/vagrant-windows/scripts/command_alias.ps1
@@ -1,6 +1,11 @@
 function which {
-    $command = [Array](Get-Command $args[0] -errorAction continue)
-    write-host  $command[0].Definition
+    $command = [Array](Get-Command $args[0] -errorAction SilentlyContinue)
+    if($null -eq $command)
+    {
+      exit 1
+    }
+    write-host $command[0].Definition
+    exit 0
 }
 
 function test ([Switch] $d, [String] $path) {
