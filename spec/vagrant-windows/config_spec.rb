5f0ff8753cbf0cf6fe81e9f5f2a0750be2d7bd4d
Mike Griffin <griffinm@gmail.com>
2013-05-02 23:44:36 +0100

    Adds tests to make sure we can override a default

--- spec/vagrant-windows/config_spec.rb
+++ spec/vagrant-windows/config_spec.rb
@@ -14,6 +14,16 @@ describe VagrantWindows::Config::Windows do
     its("halt_timeout")        { should == 30 }
     its("halt_check_interval") { should == 1 }
   end
+
+  describe "overriding defaults" do
+    [:halt_timeout, :halt_check_interval].each do |attribute|
+      it "should not default #{attribute} if overridden" do
+        instance.send("#{attribute}=".to_sym, 10)
+        instance.finalize!
+        instance.send(attribute).should == 10
+      end
+    end
+  end
 end
 
 
@@ -34,5 +44,15 @@ describe VagrantWindows::Config::WinRM do
     its("guest_port") { should == 5985 }
     its("max_tries")  { should == 12 }
     its("timeout")    { should == 1800 }
+  end
+
+  describe "overriding defaults" do
+    [:username, :password, :host, :port, :guest_port, :max_tries, :timeout].each do |attribute|
+      it "should not default #{attribute} if overridden" do
+        instance.send("#{attribute}=".to_sym, 10)
+        instance.finalize!
+        instance.send(attribute).should == 10
+      end
+    end
   end
 end
