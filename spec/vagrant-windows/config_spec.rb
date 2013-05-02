5623f58096686706755cedc94b4b9437e29b995a
Mike Griffin <griffinm@gmail.com>
2013-05-02 23:38:07 +0100

    Adds default checks for the winrm config module

--- spec/vagrant-windows/config_spec.rb
+++ spec/vagrant-windows/config_spec.rb
@@ -1,4 +1,5 @@
 require "vagrant-windows/config/windows"
+require "vagrant-windows/config/winrm"
 
 describe VagrantWindows::Config::Windows do
   let(:instance) { described_class.new }
@@ -12,5 +13,26 @@ describe VagrantWindows::Config::Windows do
 
     its("halt_timeout")        { should == 30 }
     its("halt_check_interval") { should == 1 }
+  end
+end
+
+
+describe VagrantWindows::Config::WinRM do
+  let(:instance) { described_class.new }
+
+  describe "defaults" do
+    subject do
+      instance.tap do |o|
+        o.finalize!
+      end
+    end
+
+    its("username")   { should == "vagrant" }
+    its("password")   { should == "vagrant" }
+    its("host")       { should == "localhost" }
+    its("port")       { should == 5985 }
+    its("guest_port") { should == 5985 }
+    its("max_tries")  { should == 12 }
+    its("timeout")    { should == 1800 }
   end
 end
