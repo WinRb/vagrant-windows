613c9ce34f883be69d628087da734dfd777fdf33
Gildas Cherruel <gildas.cherruel@inin.com>
2013-09-06 13:06:27 +0900

    Issue #88
    Replaced unreliable wmic with netdom

--- lib/vagrant-windows/guest/cap/change_host_name.rb
+++ lib/vagrant-windows/guest/cap/change_host_name.rb
@@ -5,7 +5,7 @@ module VagrantWindows
         def self.change_host_name(machine, name)
           #### on windows, renaming a computer seems to require a reboot
           machine.communicate.execute(
-            "wmic computersystem where name=\"%COMPUTERNAME%\" call rename name=\"#{name}\"",
+            "netdom renamecomputer \"%COMPUTERNAME%\" /NewName:\"#{name}\" /Force /Reboot:0",
             :shell => :cmd)
         end
       end
