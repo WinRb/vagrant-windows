debe0a1694e5a2083af9ea64dffebb5b78f244a4
Shawn Neal <sneal@sneal.net>
2013-10-10 18:19:28 -0700

    Split winrm and windows config tests into separate files
    Added unit test descriptor to the winrm config tests so they run with the spec rake task.

--- spec/vagrant-windows/windows_config_spec.rb
+++ spec/vagrant-windows/windows_config_spec.rb
@@ -24,34 +24,3 @@ describe VagrantWindows::Config::Windows , :unit => true do
     end
   end
 end
-
-
-describe VagrantWindows::Config::WinRM do
-  let(:instance) { described_class.new }
-
-  describe "defaults" do
-    subject do
-      instance.tap do |o|
-        o.finalize!
-      end
-    end
-
-    its("username")   { should == "vagrant" }
-    its("password")   { should == "vagrant" }
-    its("host")       { should == "localhost" }
-    its("port")       { should == 5985 }
-    its("guest_port") { should == 5985 }
-    its("max_tries")  { should == 20 }
-    its("timeout")    { should == 1800 }
-  end
-
-  describe "overriding defaults" do
-    [:username, :password, :host, :port, :guest_port, :max_tries, :timeout].each do |attribute|
-      it "should not default #{attribute} if overridden" do
-        instance.send("#{attribute}=".to_sym, 10)
-        instance.finalize!
-        instance.send(attribute).should == 10
-      end
-    end
-  end
-end
---
+++ spec/vagrant-windows/winrm_config_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe VagrantWindows::Config::WinRM, :unit => true do
+  let(:instance) { described_class.new }
+
+  describe "defaults" do
+    subject do
+      instance.tap do |o|
+        o.finalize!
+      end
+    end
+
+    its("username")   { should == "vagrant" }
+    its("password")   { should == "vagrant" }
+    its("host")       { should == "localhost" }
+    its("port")       { should == 5985 }
+    its("guest_port") { should == 5985 }
+    its("max_tries")  { should == 20 }
+    its("timeout")    { should == 1800 }
+  end
+
+  describe "overriding defaults" do
+    [:username, :password, :host, :port, :guest_port, :max_tries, :timeout].each do |attribute|
+      it "should not default #{attribute} if overridden" do
+        instance.send("#{attribute}=".to_sym, 10)
+        instance.finalize!
+        instance.send(attribute).should == 10
+      end
+    end
+  end
+end
