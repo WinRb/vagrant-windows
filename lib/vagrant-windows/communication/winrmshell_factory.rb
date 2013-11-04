6c3357c1c58f6e20331e8e914e9496622474074d
Shawn Neal <sneal@sneal.net>
2013-11-04 08:15:55 -0800

    Fixed winrmshell variable name

--- lib/vagrant-windows/communication/winrmshell_factory.rb
+++ lib/vagrant-windows/communication/winrmshell_factory.rb
@@ -7,9 +7,6 @@ module VagrantWindows
     # Factory class for generating new WinRMShell instances
     class WinRMShellFactory
 
-      attr_reader :winrmfinder
-      attr_reader :windows_machine
-
       # @param [WindowsMachine] The Windows machine instance
       # @param [WinRMFinder] The WinRMFinder instance
       def initialize(windows_machine, winrm_finder)
@@ -22,11 +19,11 @@ module VagrantWindows
       # @return [WinRMShell]
       def create_winrm_shell()
         WinRMShell.new(
-          @winrmfinder.winrm_host_address(),
+          @winrm_finder.winrm_host_address(),
           @windows_machine.winrm_config.username,
           @windows_machine.winrm_config.password,
           {
-            :port => @winrmfinder.winrm_host_port(),
+            :port => @winrm_finder.winrm_host_port(),
             :timeout_in_seconds => @windows_machine.winrm_config.timeout,
             :max_tries => @windows_machine.winrm_config.max_tries
           })
