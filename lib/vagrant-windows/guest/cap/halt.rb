49db4fb7284d43ff0a2cc6aa539ea16cb5400246
Shawn Neal <sneal@sneal.net>
2013-11-04 13:16:11 -0800

    Fix defect 129
    If there's a scheduled pending reoobt, cancel it. If we don't cancel a pending reboot then the shutdown command will fail.

--- lib/vagrant-windows/guest/cap/halt.rb
+++ lib/vagrant-windows/guest/cap/halt.rb
@@ -3,6 +3,10 @@ module VagrantWindows
     module Cap
       class Halt
         def self.halt(machine)
+          # Fix defect 129, if there's an existing scheduled reboot cancel it so shutdown succeeds
+          machine.communicate.execute("shutdown -a", :error_check => false)
+          
+          # Force shutdown the machine now
           machine.communicate.execute("shutdown /s /t 1 /c \"Vagrant Halt\" /f /d p:4:1")
 
           # Wait until the VM's state is actually powered off. If this doesn't
