d7ec03ecda9993dce14358d6d9c54287b6ecffd2
Taliesin Sisson <taliesins@yahoo.com>
2014-04-12 10:28:00 +0100

    Add support for specifying password for network share (For people who don't want guest account turned on) Add hyper-v support

--- lib/vagrant-windows/guest/cap/mount_shared_folder.rb
+++ lib/vagrant-windows/guest/cap/mount_shared_folder.rb
@@ -16,15 +16,22 @@ module VagrantWindows
         def self.mount_parallels_shared_folder(machine, name, guestpath, options)
           mount_shared_folder(machine, name, guestpath, "\\\\psf\\")
         end
+        
+        def self.mount_smb_shared_folder(machine, name, guestpath, options)
+          mount_shared_folder(machine, name, guestpath, "\\\\#{options[:smb_host]}\\", options[:smb_username], options[:smb_password])
+        end
 
         protected
         
-        def self.mount_shared_folder(machine, name, guestpath, vm_provider_unc_base)
+        def self.mount_shared_folder(machine, name, guestpath, vm_provider_unc_base, username = nil, password = nil)
           share_name = VagrantWindows::Helper.win_friendly_share_id(name)
           options = {
             :mount_point => guestpath,
             :share_name => share_name,
-            :vm_provider_unc_path => vm_provider_unc_base + share_name}
+            :vm_provider_unc_path => vm_provider_unc_base + share_name,
+            :username => username,
+            :password => password
+          }
           mount_script = VagrantWindows.load_script_template("mount_volume.ps1", :options => options)
           machine.communicate.execute(mount_script, {:shell => :powershell})
         end
